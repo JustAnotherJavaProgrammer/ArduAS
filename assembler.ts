d6cfcf9450959a0f0989a820a35e01a689b19ddc
JustAnotherJavaProgrammer <JustAnotherJavaProgrammer@users.noreply.github.com>
2021-07-17 01:20:37 +0200

    Implemented label name resolution

--- assembler.ts
+++ assembler.ts
@@ -22,10 +22,27 @@ export default class Assembler {
         const grammar: Grammar = new Map<string, Instruction>();
         for (const instrDef of lines) {
             const fields = instrDef.split("\t").map(field => field.trim());
-            const instr: Instruction = { id: parseInt(fields[0]), mnemonic: fields[1], args: fields[2]?.split(/(\s|,)+/gm).map(arg => arg.trim()).filter((arg, index) => (arg !== undefined && index !== 0 && arg?.length > 0) ?? false) ?? [] };
-            grammar.set(instr.mnemonic, instr);
+            const instr: Omit<Instruction, "parser"> = { id: parseInt(fields[0]), mnemonic: fields[1], args: fields[2]?.split(/(\s|,)+/gm).map(arg => arg.trim()).filter((arg, index) => (arg !== undefined && index !== 0 && arg?.length > 0) ?? false) ?? [] };
+            grammar.set(instr.mnemonic, this.createInstrParser(instr));
         }
         return grammar;
+    }
+
+    protected createInstrParser(instr: Omit<Instruction, "parser">): Instruction {
+        if (Assembler.isAbsoluteBranchInstruction(instr.mnemonic)) {
+            return {
+                ...instr, parser: (line: AssemblyLine, sourceFile: AssemblyFile, labels: Map<string, Label[]>,
+                    availableFiles: Map<string, AssemblyFile>, assemblerOrder: string[]) => {
+                    const args = line.code.split(/s/g).map(e => e.trim()).filter((_e, i) => i > 0);
+                    if (args.length == 0)
+                        throw new Error(`Too few arguments provided for instruction at ${line.filename}:${line.lineNo + 1} :\n${sourceFile.rawLines[line.lineNo]}`);
+                    if (labels.has(args[0]))
+                        return Assembler.createLabelResolveGenerator(instr.id, this.resolveLabelName(args[0], line, labels, availableFiles, assemblerOrder));
+                    this.warn(`Warning at ${line.filename}:${line.lineNo + 1} : Jumping to explicit addresses is discouraged. Use labels instead`);
+                }
+            }
+        }
+        return instr as Instruction;
     }
 
     async assemble(filename: string): Promise<Uint8Array> {
@@ -124,15 +141,15 @@ export default class Assembler {
         return [...this.grammar.keys()].some(mnemonic => line.startsWith(mnemonic + " "));
     }
 
-    transformAssemblyFile(sourceFile: AssemblyFile, labels: Map<string, Label[]>, availableFiles: Map<string, AssemblyFile>, assemblerOrder: string[]): TransformedAssemblyFile {
-        const generatorArr: BinaryGenerator = [];
-        for (const line of sourceFile.code) {
-            const codeSplit = line.code.split(/\s/g).map(e => e.trim()).filter(e => e.length > 0);
-            const mnemonic = codeSplit[0];
+    // transformAssemblyFile(sourceFile: AssemblyFile, labels: Map<string, Label[]>, availableFiles: Map<string, AssemblyFile>, assemblerOrder: string[]): TransformedAssemblyFile {
+    //     const generatorArr: BinaryGenerator = [];
+    //     for (const line of sourceFile.code) {
+    //         const codeSplit = line.code.split(/\s/g).map(e => e.trim()).filter(e => e.length > 0);
+    //         const mnemonic = codeSplit[0];
 
-        }
-        // TODO: implement
-    }
+    //     }
+    //     // TODO: implement
+    // }
 
     static createSimpleGenerator(result: Uint8Array): BinaryGenerator {
         return () => result;
@@ -155,7 +172,71 @@ export default class Assembler {
 
     static isAbsoluteBranchInstruction(mnemonic: string): boolean {
         mnemonic = mnemonic.trim().toUpperCase();
+        // TODO: add labels for RJMP
         return mnemonic.startsWith("BR") || mnemonic === "CALLI" || mnemonic === "JMPI";
+    }
+
+    protected resolveLabelName(labelName: string, line: AssemblyLine, labels: Map<string, Label[]>, availableFiles: Map<string, AssemblyFile>, compilationOrder: string[]): Label {
+        let candidates = labels.get(labelName) ?? [];
+        if (candidates.length == 0)
+            throw new Error(`Label "${labelName}" at ${line.filename}:${line.lineNo + 1} could not be resolved!\n${availableFiles.get(line.filename)?.rawLines[line.lineNo]}`);
+        if (candidates.length == 1)
+            return candidates[0];
+        let result = candidates[0];
+        if (candidates.some(c => c.filename === line.filename)) { // First, look for candidates in the same file
+            candidates = candidates.filter(c => c.filename === line.filename);
+            if (candidates.length == 1) {
+                result = candidates[0];
+            } else {
+                const before = candidates.filter(c => c.lineNo < line.lineNo);
+                const after = candidates.filter(c => c.lineNo > line.lineNo);
+                if (before.length > 0) // Choose the closest candidate, preferrably before the line
+                    result = before[before.length - 1];
+                else if (after.length > 0)
+                    result = after[0];
+                else
+                    // This should never happen, but if it does, we'll throw an error to stop any unexpected behavior or incompatibilities caused by an updated version not choosing the first candidate by default
+                    throw new Error(`Total confusion! There are label candidates in the same file, but neither before nor after the specified line. Labels on the same line as instructions are forbidden by language design.
+This is the position of the line, where it happened: ${line.filename}:${line.lineNo + 1} - This is the label name: "${labelName}"
+These are the candidates for the label, supposedly all in the same file:
+${candidates}`);
+            }
+        } else { // Then, look for candidates in other files
+            {
+                const toBeChecked: string[] = [line.filename];
+                const alreadyChecked: string[] = [];
+                for (const fileCandidate of toBeChecked) { // Going through all dependencies, level by level
+                    if (candidates.some(c => c.filename === fileCandidate)) {
+                        candidates = candidates.filter(c => c.filename === fileCandidate);
+                        result = candidates[candidates.length - 1];
+                        break;
+                    }
+                    alreadyChecked.push(fileCandidate);
+                    toBeChecked.push(...(availableFiles.get(fileCandidate)?.dependencies ?? []).filter(e => !alreadyChecked.includes(e)).reverse());
+                }
+            }
+            // Otherwise, choose the closest candidate in all assembled files, preferrably before the line
+            const ownCompOrderIndex = compilationOrder.indexOf(line.filename);
+            const before = compilationOrder.filter((_e, i) => i < ownCompOrderIndex).filter(e => candidates.some(c => c.filename === e));
+            const after = compilationOrder.filter((_e, i) => i > ownCompOrderIndex).filter(e => candidates.some(c => c.filename === e));
+            if (before.length > 0) {
+                candidates = candidates.filter(c => c.filename === before[before.length - 1]);
+                result = candidates[candidates.length - 1];
+            } else if (after.length > 0) {
+                candidates = candidates.filter(c => c.filename === after[0]);
+                result = candidates[0];
+            }
+            // If nothing works, choose the first candidate
+        }
+        this.warn(`Multiple candidates for label "${labelName}" at ${line.filename}:${line.lineNo + 1} - Choosing candidate at ${result.filename}:${result.lineNo + 1}`);
+        return result;
+    }
+
+    warn(warning: string) {
+        const oldText = this.spinner.text;
+        this.spinner.warn(warning);
+        this.spinner.text = oldText;
+        this.spinner.start();
     }
 }
 
@@ -164,6 +245,9 @@ type Grammar = Map<string, Instruction>;
 type AssemblyLine = { code: string, lineNo: number, filename: string };
 
 type TransformedAssemblyLine = AssemblyLine & { generator: BinaryGenerator };
+
+type InstructionParser = (line: AssemblyLine, sourceFile: AssemblyFile, labels: Map<string, Label[]>,
+    availableFiles: Map<string, AssemblyFile>, assemblerOrder: string[]) => BinaryGenerator;
 
 type BinaryGenerator = (labels: Map<Label, number>) => Uint8Array;
 
@@ -176,7 +260,8 @@ interface Label {
 interface Instruction {
     id: number;
     mnemonic: string,
-    args: string[]
+    args: string[],
+    parser: InstructionParser
 }
 
 interface AssemblyFile {
