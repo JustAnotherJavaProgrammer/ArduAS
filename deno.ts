c4853654150dd9bf3cb9a03de1204033ab249210
JustAnotherJavaProgrammer <JustAnotherJavaProgrammer@users.noreply.github.com>
2021-07-19 00:46:00 +0200

    A "time taken" info to the final console output

--- deno.ts
+++ deno.ts
@@ -8,6 +8,7 @@ if (Deno.build.os === "windows") {
 https://stackoverflow.com/questions/57131654/using-utf-8-encoding-chcp-65001-in-command-prompt-windows-powershell-window/57134096#57134096`);
 }
 
+const t0 = performance.now();
 const spinner = wait("Loading grammar...").start();
 const assembler: Assembler = await (async () => {
     const grammar = await Deno.readTextFile("./grammar.txt");
@@ -18,8 +19,8 @@ const assembler: Assembler = await (async () => {
     return new Assembler(async (filename) => decoder.decode(await Deno.readFile(filename)), path => resolve(normalize(path)), grammar, spinner);
 })();
 
-const sourceFile = "hello_world.asm";
-const outputFile = "HELLO.RUN";
+const sourceFile = Deno.args[0];
+const outputFile = Deno.args[1] ?? (sourceFile.substring(0, sourceFile.lastIndexOf(".")).toUpperCase() + ".RUN");
 spinner.text = `Assembling ${sourceFile} ...`; spinner.color = "magenta";
 spinner.start();
 spinner.info();
@@ -35,6 +36,7 @@ try {
     spinner.succeed();
     spinner.succeed("Done!");
     spinner.info(`Binary size: ${binaryData.length} bytes`);
+    spinner.info(`Time taken: ${performance.now()-t0}ms`);
 } catch (e) {
     spinner.indent = 0;
     spinner.fail(`Compilation failed: ${e.message}`);
